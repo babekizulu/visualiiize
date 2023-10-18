fc3a4a5b6c4e4eeb3eeb15e0557e285a852f8d80
BABEKIZULU <babekizulu@gmail.com>
2023-10-18 11:39:29 +0200

    ViSUALiiiZE Setup *LIGHTNING EMOJII* *HEART EMOJiii*

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+//libs
+import React from 'react';
+//components
+import Header from 'header';
+import Route from './Route';
+const App = () => {
+  return (
+    <div className='app-container'>
+      <Header />
+      <Route></Route>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Logo.js
@@ -0,0 +1,16 @@
+//libs
+import React from 'react';
+//components
+import Link from './Link';
+
+const Logo = () => {
+  return (
+    <Link href='/' className='logo-container'>
+      <header className='logo'>
+        <h1>VISUALiiiZE</h1>
+      </header>
+    </Link>
+  );
+};
+
+export default Logo;
